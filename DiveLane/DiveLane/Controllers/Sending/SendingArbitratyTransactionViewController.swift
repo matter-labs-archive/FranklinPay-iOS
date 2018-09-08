3b45a50a9bed6899d45699e0bb2529fd70b4e331
Georgii Fesenko <fesenko.g@gmail.com>
2018-09-08 13:27:07 +0200

    etherscan service added, arbitrary transaction sending interface created

---
+++ DiveLane/DiveLane/Controllers/Sending/SendingArbitratyTransactionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SendingArbitratyTransactionViewController.swift
+//  DiveLane
+//
+//  Created by Georgii Fesenko on 08/09/2018.
+//  Copyright © 2018 Matter Inc. All rights reserved.
+//
+
+import UIKit
+import web3swift
+
+class SendArbitraryTransactionViewController: UIViewController {
+    
+    @IBOutlet weak var balanceOnWalletTextField: UILabel!
+    @IBOutlet weak var fromTextField: UILabel!
+    @IBOutlet weak var contractAddressTextField: UITextField!
+    @IBOutlet weak var gasPriceTextField: UITextField!
+    @IBOutlet weak var gasLimitTextField: UITextField!
+    @IBOutlet weak var methodNameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var params: [Parameter]
+    var transactionInfo: TransactionInfo
+    
+    init(params: [Parameter], transactionInfo: TransactionInfo) {
+        self.params = params
+        self.transactionInfo = transactionInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let nib = UINib.init(nibName: "ParameterCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "ParameterCell")
+        
+        //TODO: - Setup outlets
+    }
+    
+    
+    @IBAction func sendButtonWasTapped(_ sender: Any) {
+        //TODO: - Password
+        TransactionsService().sendToContract(transaction: transactionInfo.transactionIntermediate, with: "") { (result) in
+            switch result {
+            case .Error(let error):
+                print(error)
+            case .Success(let success):
+                print(success)
+                //TODO: - Something
+            }
+        }
+    }
+    
+}
+
+extension SendArbitraryTransactionViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return params.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ParameterCell", for: indexPath) as? ParameterCell else { return UITableViewCell() }
+        cell.configure(params[indexPath.row])
+        return cell
+    }
+    
+}
---
+++ DiveLane/DiveLane/Services/EtherscanService.swift
@@ -0,0 +1,58 @@
+//
+//  EtherscanService.swift
+//  DiveLane
+//
+//  Created by Georgii Fesenko on 08/09/2018.
+//  Copyright © 2018 Matter Inc. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class EtherscanService {
+    func getAbi(forContractAddress contractAddress: String, completion: @escaping(Result<String>) -> Void) {
+        let urlString = "https://api.etherscan.io/api?module=contract&action=getabi&address=\(contractAddress)&apikey=YourApiKeyToken"
+        guard let url = URL(string: urlString) else {
+            completion(Result.Error(NetworkErrors.couldnotParseUrlString))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, responce, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(Result.Error(error))
+                }
+                return
+            }
+            if let data = data {
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else {
+                        DispatchQueue.main.async {
+                            completion(Result.Error(NetworkErrors.couldnotParseJSON))
+                        }
+                        return
+                    }
+                    if let message = json["message"], message == "OK", let abi = json["result"] {
+                        DispatchQueue.main.async {
+                            completion(Result.Success(abi))
+                        }
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(Result.Error(error))
+                    }
+                }
+            }
+            
+        }
+        dataTask.resume()
+        
+    }
+}
+
+enum NetworkErrors: Error {
+    case couldnotParseUrlString
+    case couldnotParseJSON
+}
