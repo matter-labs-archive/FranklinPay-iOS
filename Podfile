72af270a467f6d86ef23e06c690651b06446736e
Volodymyr <klymenkodp@gmail.com>
2018-10-01 22:13:06 -0400

    Installed Alamofire to the project

--- Podfile
+++ Podfile
@@ -9,5 +9,6 @@ target 'DiveLane' do
   pod 'QRCodeReader.swift', '~> 8.1.1'
   pod 'Fabric'
   pod 'Crashlytics'
+  pod 'Alamofire', '~> 4.7'
 
 end
--- Pods/Target Support Files/Pods-DiveLane/Pods-DiveLane-frameworks.sh
+++ Pods/Target Support Files/Pods-DiveLane/Pods-DiveLane-frameworks.sh
@@ -143,6 +143,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BigInt/BigInt.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
@@ -154,6 +155,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/web3swift/web3swift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BigInt/BigInt.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
