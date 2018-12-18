90200d26daf23cba5e9ba8e0e634784031c09cf5
Anton Grigorev <antongrigorjev2010@gmail.com>
2018-12-18 20:37:51 +0300

    fixed ui in wallets dropdown on sending

--- DiveLane/Presentation Layer/Views/Sending/Cells/WalletCellDropdown.swift
+++ DiveLane/Presentation Layer/Views/Sending/Cells/WalletCellDropdown.swift
@@ -10,7 +10,6 @@ import UIKit
 import EthereumAddress
 
 class WalletCellDropdown: UITableViewCell {
-    @IBOutlet weak var walletBalance: UILabel!
     @IBOutlet weak var walletAddress: UILabel!
     @IBOutlet weak var walletName: UILabel!
 
@@ -21,28 +20,10 @@ class WalletCellDropdown: UITableViewCell {
         currentWallet = wallet
         walletAddress.text = wallet.address.hideExtraSymbolsInAddress()
         walletName.text = wallet.name
-        do {
-            if let token = CurrentToken.currentToken {
-                let balance = try
-                    web3SwiftService.getERC20balance(for: wallet,
-                                                     token: token)
-                if let currentAddress = self.currentWallet?.address, currentAddress == wallet.address {
-                    self.walletBalance.text = balance + " " + (CurrentToken.currentToken?.symbol.uppercased() ?? "")
-                }
-            } else {
-                let balance = try web3SwiftService.getETHbalance(for: wallet)
-                if let currentAddress = self.currentWallet?.address, currentAddress == wallet.address {
-                    self.walletBalance.text = balance + " ETH"
-                }
-            }
-        } catch {
-            return
-        }
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        walletBalance.text = "Loading..."
     }
 
 }
